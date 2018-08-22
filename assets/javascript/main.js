7063d294abcd5c9445d4ce9f96dd7c1c860dab36
Nick Sedillos <nick.sedillos@gmail.com>
2018-08-21 19:15:41 -0500

    Added Firebase boilerplate and linked to main.js.

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD_a_1FWqhyWixQ1kusQNchcDb-MUrbLWk",
+    authDomain: "home-inventory-manager.firebaseapp.com",
+    databaseURL: "https://home-inventory-manager.firebaseio.com",
+    projectId: "home-inventory-manager",
+    storageBucket: "home-inventory-manager.appspot.com",
+    messagingSenderId: "165919673421"
+  };
+  firebase.initializeApp(config);
