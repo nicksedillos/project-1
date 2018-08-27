4dd64414a23a5280ef1944c76272b8a22e0c1c69
Nick Sedillos <nick.sedillos@gmail.com>
2018-08-26 19:10:30 -0500

    Comments on UPC lookup.

--- assets/javascript/upc-lookup.js
+++ assets/javascript/upc-lookup.js
@@ -1,3 +1,9 @@
 const apiKey = o35yk1ejzwscfqtoq9p0hucufz3okk;
 var barcode = ""
 var queryURL = "https://api.barcodelookup.com/v2/products?barcode=" + barcode + "&key=" + apiKey;
+
+/*
+When a UPC code is added, make a query to Barcode Lookup.
+Record the entire result to the parent item in Firebase.
+Log this result to console.
+*/
